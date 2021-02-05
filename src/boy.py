979861b0b36c15f5ed5a5f3d4610b4a304322a73
“rafavg77” <rafavg77@gmail.com>
2021-02-05 00:44:58 -0600

    Security functionality is added to only answer to allowed users and systemd script is added

--- src/boy.py
+++ src/boy.py
@@ -6,6 +6,7 @@ from Utils.getSpeedTest import GetSpeed
 from Utils.getPiHole import *
 from Utils.firewallUtil import *
 from configparser import ConfigParser
+import time
 import os
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)#logger = logging.getLogger(__name__)
@@ -16,73 +17,95 @@ initfile = os.path.join(thisfolder, 'Utils/config/config.ini')
 config = ConfigParser()
 config.read(initfile)
 token_bot = config.get('auth', 'token_bot')
+bad_permission = config.get('users', 'reply')
 
+
+def isPermited(bot, update):
+    id = bot.message.chat.id
+    PERMITED_USERS = config.get('users',"permited")
+    if str(id) in PERMITED_USERS:
+        permission = True
+    else:
+        permission = False
+        bot.message.reply_text(bad_permission)
+
+    return permission 
 
 def start(bot, update):
-    logger.info('I have received a /start command')
-    bot.message.reply_text("Hi Master, I'm here to server you!! ")
+    if isPermited(bot, update):
+        logger.info('I have received a /start command')
+        bot.message.reply_text("Hi Master, I'm here to server you!! ")
                             #reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
 def ping(bot, update):
     logger.info('I have received a /ping command')
-    bot.message.reply_text("Pong !!")
+    if isPermited(bot, update):
+        bot.message.reply_text("Pong !!")
 
 def publicip(bot, update):
     logger.info('I have received a /publicip command')
-    info = GetPublicIP()
-    response = "Public IP: " + info.publicIP.ip + "\nCity: " + info.publicIP.city + "\nRegion: " + info.publicIP.region + "\nCountry: " + info.publicIP.country + "\nOrg: " + info.publicIP.org
-    bot.message.reply_text(response)
+    if isPermited(bot, update):
+        info = GetPublicIP()
+        response = "Public IP: " + info.publicIP.ip + "\nCity: " + info.publicIP.city + "\nRegion: " + info.publicIP.region + "\nCountry: " + info.publicIP.country + "\nOrg: " + info.publicIP.org
+        bot.message.reply_text(response)
 
 def getspeed(bot, update):
     logger.info('I have received a /getspeed command')
-    speed = GetSpeed()
-    response = "Ping: " + speed.ping + "\nDonwload: " + speed.speedTestInfo.download + "\nUpload: " + speed.speedTestInfo.upload
-    bot.message.reply_text(response)
+    if isPermited(bot, update):
+        speed = GetSpeed()
+        response = "Ping: " + speed.ping + "\nDonwload: " + speed.speedTestInfo.download + "\nUpload: " + speed.speedTestInfo.upload
+        bot.message.reply_text(response)
 
 def piholestatus(bot, update):
     logger.info('I have received a /piholestatus command')
-    statusHole = getHoleStatus()
-    bot.message.reply_text("PiHole Status: " + statusHole)
+    if isPermited(bot, update):
+        statusHole = getHoleStatus()
+        bot.message.reply_text("PiHole Status: " + statusHole)
 
 def pihodisable(bot, update):
     logger.info('I have received a /pihodisable command')
-    setHoleDisable()
-    bot.message.reply_text("Setting PiHole: Disable")
+    if isPermited(bot, update):
+        setHoleDisable()
+        bot.message.reply_text("Setting PiHole: Disable")
 
 def piholeenable(bot, update):
     logger.info('I have received a /piholeenable command')
-    setHoleEnable()
-    bot.message.reply_text("Setting PiHole: Enable")
+    if isPermited(bot, update):
+        setHoleEnable()
+        bot.message.reply_text("Setting PiHole: Enable")
 
 def fwGetInterfaces(bot, update):
     logger.info('I have received a /fwGetInterfaces command')
-    fw = FirewallApi()
-    status = fw.getAllWANStatus()
-    print(status)
-    bot.message.reply_text("Firwall Interfaces: " + status)
+    if isPermited(bot, update):
+        fw = FirewallApi()
+        status = fw.getAllWANStatus()
+        print(status)
+        bot.message.reply_text("Firwall Interfaces: " + status)
 
 def fwEnableWAN(bot, update):
-    wanInterface = bot.message.text.replace("/fwEnableWAN", "").strip()
-    print(wanInterface)
-    if int(wanInterface) < 3:    
-        logger.info('I have received a /fwEnableWAN command')
-        fw = FirewallApi()
-        status = fw.enableWan(int(wanInterface))
-        print(status)
-        bot.message.reply_text("Firwall Enabling WAN: " + wanInterface)
-    else:
-        bot.message.reply_text("Wrong Interface Number")
+    logger.info('I have received a /fwEnableWAN command')
+    if isPermited(bot, update):
+        wanInterface = bot.message.text.replace("/fwEnableWAN", "").strip()
+        print(wanInterface)
+        if int(wanInterface) < 3:    
+            fw = FirewallApi()
+            status = fw.enableWan(int(wanInterface))
+            time.sleep(10)
+            bot.message.reply_text("Firwall Enabling WAN: " + wanInterface)
+        else:
+            bot.message.reply_text("Wrong Interface Number")
 
 def fwDisableWAN(bot, update):
-    wanInterface = bot.message.text.replace("/fwDisableWAN", "").strip()
-    if int(wanInterface) < 3:    
-        logger.info('I have received a /fwDisableWAN command')
-        fw = FirewallApi()
-        status = fw.disableWan(int(wanInterface))
-        print(status)
-        bot.message.reply_text("Firwall Disabling WAN: " + wanInterface)
-    else:
-        bot.message.reply_text("Wrong Interface Number")
+    logger.info('I have received a /fwDisableWAN command')
+    if isPermited(bot, update):
+        wanInterface = bot.message.text.replace("/fwDisableWAN", "").strip()
+        if int(wanInterface) < 3:    
+            fw = FirewallApi()
+            status = fw.disableWan(int(wanInterface))
+            time.sleep(10)
+            bot.message.reply_text("Firwall Disabling WAN: " + wanInterface)
+        else:
+            bot.message.reply_text("Wrong Interface Number")
 
 if __name__ == '__main__':
 
